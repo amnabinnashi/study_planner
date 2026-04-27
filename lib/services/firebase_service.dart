e2dc09e58a4b47868efbfaf090c7cb9c77d7dffd
Amna Bin Nashi <amnabinnashi2000@gmail.com>
2026-04-27 23:19:49 +0300

    Add FirebaseService with subject management methods
    Implemented CRUD operations for subjects in Firebase.

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,1 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class FirebaseService {
+  final _db = FirebaseFirestore.instance;
+
+  Stream<QuerySnapshot> getSubjects() {
+    return _db.collection('subjects').snapshots();
+  }
+
+  Future<void> addSubject(String name, int hours) async {
+    await _db.collection('subjects').add({
+      'name': name,
+      'totalHours': hours,
+      'doneHours': 0,
+    });
+  }
+
+  Future<void> updateProgress(String id, int doneHours) async {
+    await _db.collection('subjects').doc(id).update({
+      'doneHours': doneHours,
+    });
+  }
+
+  Future<void> deleteSubject(String id) async {
+    await _db.collection('subjects').doc(id).delete();
+  }
+}
