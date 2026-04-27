ab66746a5eec670591fe01e35be225a780e7bb73
Amna Bin Nashi <amnabinnashi2000@gmail.com>
2026-04-27 23:20:27 +0300

    Implement StudyProvider with Firebase service methods

--- lib/providers/study_provider.dart
+++ lib/providers/study_provider.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import '../services/firebase_service.dart';
 
+class StudyProvider extends ChangeNotifier {
+  final FirebaseService _service = FirebaseService();
+
+  Stream get subjectsStream => _service.getSubjects();
+
+  Future<void> addSubject(String name, int hours) async {
+    await _service.addSubject(name, hours);
+  }
+
+  Future<void> addProgress(
+      String id, int doneHours, int totalHours) async {
+    if (doneHours < totalHours) {
+      await _service.updateProgress(id, doneHours + 1);
+    }
+  }
+
+  Future<void> deleteSubject(String id) async {
+    await _service.deleteSubject(id);
+  }
+}
